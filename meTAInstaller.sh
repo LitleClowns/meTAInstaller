281364f5807419ff806a78e8009ef7080ada6af1
Joker's <49826385+LitleClowns@users.noreply.github.com>
2019-04-24 14:17:50 +0700

    Add files via upload

---
+++ meTAInstaller.sh
@@ -0,0 +1,84 @@
+#Mau ngapain Lu bangsad
+#jangan nge-recode anjing
+#gw buat susah-susah
+#mau gw pukulin lo
+#Youtube : Creater Clown
+#Sukreb ea
+
+apt-get update -y
+
+apt-get upgrade -y
+
+apt-get install ruby -y
+
+apt-get install toilet -y
+
+apt-get install cowsay -y
+
+apt-get install figlet -y 
+
+gem install lolcat -y 
+
+
+clear
+
+toilet -f smslant -F gay 'meTAInstaller'
+sleep 1
+echo "++++++++++++++++++ === ++++++++++++++++++" | lolcat
+echo "Installer Metasploit-Framework" | lolcat
+echo "Create by : Joker•S" | lolcat
+sleep 1
+echo "Subkreb Youtube : Creater Clown" | lolcat
+sleep 1
+echo "++++++++++++++++++ === ++++++++++++++++++" | lolcat
+sleep 3
+
+clear
+
+echo ""
+echo "BAHAN - BAHAN " | lolcat
+sleep 1
+echo "Harus Terkoneksi ke INTERNET" | lolcat
+sleep 1
+echo "Sisakan ruang minimal 500mb" | lolcat
+sleep 1
+echo "Tidak boleh keluar dari apk TERMUX" |lolcat
+sleep 1
+echo "Proses Instaling 5-15mnt" | lolcat
+echo ""
+
+read -p "Siap? [Y/T]= " cia
+
+
+echo "Waiting for progress ...." | lolcat
+sleep 3
+
+case $cia in
+
+apt-get install git -y
+
+apt-get install wget -y
+
+apt-get install curl -y
+
+apt-get install ruby -y
+
+gem install bundle -y
+
+apt-get install root-repo -y
+
+apt-get unstable root-repo -y
+
+apt-get install x11-repo -y
+
+apt-get install metasploit
+
+echo "Metasploit sudah TerInstall" | lolcat
+
+
+cd $HOME
+
+clear
+
+metasploit
+
