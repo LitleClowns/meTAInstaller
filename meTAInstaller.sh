2ba83f186c047af93c352e0e5b46e4a079234039
Joker's <49826385+LitleClowns@users.noreply.github.com>
2019-04-25 08:00:16 +0700

    Update meTAInstaller.sh

--- meTAInstaller.sh
+++ meTAInstaller.sh
@@ -47,7 +47,7 @@ sleep 1
 echo "Proses Instaling 5-15mnt" | lolcat
 echo ""
 
-read -p "Siap? [Y/T]= " cia
+read -p "Siap? = Y / T" cia
 
 
 echo "Waiting for progress ...." | lolcat
@@ -55,7 +55,7 @@ sleep 3
 
 case $cia in
 
-Y)cd $HOME
+y)cd $HOME
 apt-get install git -y && apt-get install wget -y
 apt-get install curl -y
 apt-get install ruby -y
